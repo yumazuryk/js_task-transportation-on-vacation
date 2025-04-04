a20d19d5640e23b35f2b9c431a4d7aabfb338f93
Yuliia Mazuryk <yuliapikh53@gmail.com>
2025-04-04 15:46:17 +0200

    function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
-}
+  const pricePerDay = 40;
+  const basePrice = days * pricePerDay;
+  const longTerm = 7;
+  const longTermDiscount = 50;
+  const mediumTerm = 3;
+  const mediumTermDiscount = 20;
 
+  if (days >= longTerm) {
+    return basePrice - longTermDiscount;
+  }
+
+  if (days >= mediumTerm) {
+    return basePrice - mediumTermDiscount;
+  }
+
+  return basePrice;
+}
 module.exports = calculateRentalCost;
